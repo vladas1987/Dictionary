81795ef300d8b1bf8068d67f5d132ad36a65d177
vladas1987 <petras18@gmail.com>
2017-04-18 12:47:00 -0400

    C# Dictionary

---
+++ DictionaryExample/DictionaryExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> myDictionary = new Dictionary<string, string>();
+
+            myDictionary.Add("key1", "First Item");
+            myDictionary.Add("key2", "Second Item");
+            myDictionary.Add("key3", "Third Item");
+            myDictionary.Add("key4", "Fourth Item");
+            myDictionary.Add("key5", "Fifth Item");
+
+            Console.WriteLine("Item represented by the key 'key2' : " + myDictionary["key2"]);
+
+        }
+    }
+}
